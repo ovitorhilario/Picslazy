caef8b7bf732114000543d14b8f4e5035982d3e7
ovitorhilario <meditous@gmail.com>
2023-08-14 16:54:48 -0300

    refactor: top menu content

--- app/src/main/java/com/vitorhilarioapps/picslazy/common/designsystem/widgets/TopBar.kt
+++ app/src/main/java/com/vitorhilarioapps/picslazy/common/designsystem/widgets/TopBar.kt
@@ -4,28 +4,25 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.KeyboardArrowLeft
 import androidx.compose.material.icons.rounded.Menu
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.airbnb.lottie.compose.LottieAnimation
@@ -37,7 +34,7 @@ import com.vitorhilarioapps.picslazy.common.designsystem.theme.Typography
 
 @Composable
 fun TopBarNavigateBack(
-    onClick: () -> Unit,
+    onClick: () -> Unit
 ) {
     Row(
         modifier = Modifier
@@ -56,14 +53,46 @@ fun TopBarNavigateBack(
 @Composable
 fun TopBarMenu(
     onClick: () -> Unit,
+    textCenter: String? = null
 ) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
             .height(64.dp)
             .padding(8.dp),
-        horizontalArrangement = Arrangement.End
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
     ) {
+
+        Box(
+            modifier = Modifier
+                .size(48.dp),
+        ) {
+            val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.animation_profile))
+
+            LottieAnimation(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .align(Alignment.Center),
+                composition = composition,
+                iterations = LottieConstants.IterateForever,
+                contentScale = ContentScale.Crop
+            )
+        }
+
+        if (textCenter != null) {
+            Text(
+                modifier = Modifier.padding(start = 8.dp).weight(1f),
+                text = textCenter,
+                textAlign = TextAlign.Start,
+                fontStyle = Typography.titleSmall.fontStyle,
+                fontWeight = FontWeight.Medium,
+                fontSize = Typography.titleSmall.fontSize,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+
         IconExtended(
             imageVector = Icons.Rounded.Menu,
             onClick = onClick
@@ -140,5 +169,5 @@ fun PreviewTopBarActions() {
 @Preview
 @Composable
 fun PreviewTopBarMenu() {
-    TopBarMenu {}
+    TopBarMenu({}, "Hello digiar")
 }
