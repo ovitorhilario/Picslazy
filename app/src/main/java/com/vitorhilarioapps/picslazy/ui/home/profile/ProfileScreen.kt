272178316cf94e6e492607be54287b04e708e590
ovitorhilario <meditous@gmail.com>
2023-08-14 17:00:06 -0300

    style: remove unnecessary imports

--- app/src/main/java/com/vitorhilarioapps/picslazy/ui/home/profile/ProfileScreen.kt
+++ app/src/main/java/com/vitorhilarioapps/picslazy/ui/home/profile/ProfileScreen.kt
@@ -1,7 +1,5 @@
 package com.vitorhilarioapps.picslazy.ui.home.profile
 
-import android.net.Uri
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -11,20 +9,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.KeyboardArrowLeft
-import androidx.compose.material.icons.rounded.Menu
-import androidx.compose.material.icons.rounded.Person
 import androidx.compose.material3.Divider
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -33,11 +23,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -45,8 +31,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.LottieConstants
@@ -55,8 +39,6 @@ import com.vitorhilarioapps.picslazy.R
 import com.vitorhilarioapps.picslazy.common.designsystem.theme.Typography
 import com.vitorhilarioapps.picslazy.common.designsystem.theme.mRed
 import com.vitorhilarioapps.picslazy.common.designsystem.theme.mRedLight
-import com.vitorhilarioapps.picslazy.common.designsystem.widgets.IconExtended
-import com.vitorhilarioapps.picslazy.common.designsystem.widgets.TopBarMenu
 import com.vitorhilarioapps.picslazy.common.designsystem.widgets.TopBarNavigateBack
 import kotlinx.coroutines.launch
 
@@ -101,7 +83,6 @@ fun ProfileScreen(
                     contentScale = ContentScale.Crop
                 )
             }
-
 
             Spacer(modifier = Modifier.fillMaxWidth())
 
